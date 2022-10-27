551e6a53074a89dcb24ce610594c7de7751ba976
Jan Gru <j4n6ru@gmail.com>
2022-10-27 16:30:03 +0200

    Switch to myst_parser

--- docs/conf.py
+++ docs/conf.py
@@ -29,17 +29,19 @@ sys.path.insert(0, os.path.abspath('../collectors'))
 #
 # needs_sphinx = '1.0'
 
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {'.md': CommonMarkParser}
-source_suffix = ['.rst', '.md']
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     "sphinx.ext.autodoc",  # Core library for html generation from docstrings
+    'myst_parser'
 ]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
