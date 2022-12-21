0f2357c983932ba51a6b27fd92c39d442e70e818
Jan Gru <j4n6ru@gmail.com>
2022-12-21 09:39:40 +0100

    Add path correction

--- periphery/fix-permissions.sh
+++ periphery/fix-permissions.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Script to create bind mounted dirs and fix permissions thereof
 #
+parent=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent"
+
 mkdir -p mongodb/mongodb_data/
 sudo chown -R 1001 mongodb/mongodb_data/
 mkdir -p elasticstack/elasticsearch/data
